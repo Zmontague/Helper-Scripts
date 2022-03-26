b836e36be7aab670231e42397302cc9d6702d3cb
Zachary Montague <ZacharyRMontague@gmail.com>
2022-03-26 13:32:49 -0500

    Update monitoringCurlHit.sh

--- monitoringCurlHit.sh
+++ monitoringCurlHit.sh
@@ -1,3 +1,8 @@
+# Replace "server address" with IP/url of server. Easy to use a busy box on kubernetes and run the command. 
+# Easy K8s setup: k run curl-test --image=radial/busyboxplus:curl -i --tty --rm
+# k is kubectl setup as my normal alias, these commands will start the pod as interactive and remove when finished. 
+
+
 for i in $(seq 1 10000)
 do
   curl "server address" > curlTest.txt
